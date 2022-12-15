e56f210aeb1172ccd55c65225a33bbca1f628a6b
MARCOS NAVARRO MARTÍN <navmarma@gregoriofer.local>
2022-12-15 13:16:17 +0100

    Transition page

--- scripts/Logic.js
+++ scripts/Logic.js
@@ -136,3 +136,14 @@ function createTable(e) {
     }
   }
 }
+
+window.transitionToPage = function (href) {
+  document.querySelector("body").style.opacity = 0;
+  setTimeout(function () {
+    window.location.href = href;
+  }, 500);
+};
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.querySelector("body").style.opacity = 1;
+});
